f4b423fec8f4dacc3c7d30af12ec8e6ecef4c764
Michal Jan Warecki <mwarecki1@gmail.com>
2024-04-05 17:04:19 +0200

    Initial commit.

---
+++ app/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    # path('', Login.as_view(), name='login'),
+    # path('stores', StoreList, name='store_list'),
+    # path('stores/<store_id>', StoreView, name='store'),
+    # path('groceries', GroceryGroupList, name='grocery_group_list'),
+    # path('groceries/<group>', GroceryGroupView, name='grocery_group'),
+    # path('groceries/<group>/<grocery>', GroceryView, name='grocery'),
+    # path('brands', BrandList, name='brand_list'),
+    # path('brands/<brand_id>', BrandView, name='brand'),
+    # path('receipt', ReceiptList, name='receipt_list'),
+    # path('receipt/<brand_id>', ReceiptView, name='receipt'),
+]
