96be3c34db4428fd6b06fae8736e2c2646538e71
Michal Jan Warecki <mwarecki1@gmail.com>
2024-04-06 11:05:44 +0200

    Basic models for a receipt.

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from .models import *
+
+
+class PurchaseInline(admin.TabularInline):
+    model = Purchase
+
+
+class ReceiptAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (
+            None,
+            {
+                "fields": ['store', 'date', ]
+            }
+        )
+    ]
+    inlines = [
+        PurchaseInline,
+    ]
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'brand']
+
 
 # Register your models here.
+admin.site.register(Store)
+admin.site.register(Brand)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Receipt, ReceiptAdmin)
--- app/models.py
+++ app/models.py
@@ -1,3 +1,51 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Brand(models.Model):
+    name = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.name
+
+
+class Store(models.Model):
+    name = models.CharField(max_length=250)
+    address = models.CharField(max_length=250, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=250)
+    parent = models.ForeignKey('self', blank=True, null=True,
+                               on_delete=models.CASCADE)
+    brand = models.ForeignKey(Brand, blank=True, null=True,
+                              on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return self.name
+
+
+class Receipt(models.Model):
+    date = models.DateTimeField()
+    store = models.ForeignKey(Store,
+                              on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "%.2f kr, %s" % (
+            sum([x.total_price for x in self.purchases.all()])/100.0,
+            self.date
+        )
+
+
+class Purchase(models.Model):
+    product = models.ForeignKey(Product,
+                                on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=7, decimal_places=3)
+    total_price = models.IntegerField()
+    discount = models.IntegerField()
+    receipt = models.ForeignKey(Receipt, related_name="purchases",
+                                on_delete=models.CASCADE)
