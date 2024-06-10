dca27cb136a38b62df18efde449eec431b455962
Michal Jan Warecki <mwarecki1@gmail.com>
2024-06-10 23:39:12 +0200

    Receipt styles

--- wsgi/app/templatetags/boutique.py
+++ wsgi/app/templatetags/boutique.py
@@ -3,3 +3,4 @@ from django import template
 register = template.Library()
 
 register.filter("currency", lambda x: '{:.2f} NOK'.format(x).replace(".", ","))
+register.filter("minor_currency", lambda x: '{:.2f} NOK'.format(x/100).replace(".", ","))
