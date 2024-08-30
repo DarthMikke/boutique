a71c4ae3095a520c0a2757c5c689d423a53fe9eb
Michal Jan Warecki <mwarecki1@gmail.com>
2024-08-30 09:21:26 +0200

    Use envvars in the build process.

--- tools/build.sh
+++ tools/build.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+source sample.env
+
 CWD=$(pwd | sed 's/\//\n/g' | tail -n 1)
 
 if [ $CWD == "tools" ]
@@ -12,6 +14,7 @@ TAG=ghcr.io/darthmikke/boutique:$(git log -n 1 --format="%h")
 docker build \
 	-t $TAG \
 	-f tools/Dockerfile \
+ 	--build-arg DB_NAME=$DB_NAME \
 	.
 
 echo "Built $TAG"
