2bbf2ccd537731307f6fdf6ce9bb25093ed536d5
Michal Jan Warecki <mwarecki1@gmail.com>
2024-08-31 22:23:08 +0200

    ASGI: Read from dotenv

--- wsgi/boutique/asgi.py
+++ wsgi/boutique/asgi.py
@@ -8,8 +8,14 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 """
 
 import os
+import dotenv
 
 from django.core.asgi import get_asgi_application
+
+env_file_path = os.path.join(
+    os.path.dirname(os.path.dirname(__file__)), '.env'
+)
+dotenv.read_dotenv(env_file_path)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'boutique.settings')
 
